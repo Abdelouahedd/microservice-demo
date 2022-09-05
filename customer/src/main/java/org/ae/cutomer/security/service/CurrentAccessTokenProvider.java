a45d538f305da0f60e2f5de53db7f561204a777e
Abdelouahedd <ennouriabdelouahed@gmail.com>
2022-09-05 15:29:21 +0100

    feat(security): add keycloak.yml docker file, and create security configuration for customer service

---
+++ customer/src/main/java/org/ae/cutomer/security/service/CurrentAccessTokenProvider.java
@@ -0,0 +1,104 @@
+package org.ae.cutomer.security.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+@Log4j2
+public class CurrentAccessTokenProvider {
+  /**
+   * Get the login of the current user.
+   *
+   * @return the login of the current user.
+   */
+  public static Optional<String> getCurrentUserLogin() {
+    SecurityContext securityContext = SecurityContextHolder.getContext();
+    return Optional.ofNullable(extractPrincipal(securityContext.getAuthentication()));
+  }
+
+  private static String extractPrincipal(Authentication authentication) {
+    if (authentication == null) {
+      return null;
+    } else if (authentication.getPrincipal() instanceof UserDetails) {
+      UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+      return springSecurityUser.getUsername();
+    } else if (authentication instanceof JwtAuthenticationToken) {
+      return (String) ((JwtAuthenticationToken) authentication).getToken().getClaims().get("preferred_username");
+    } else if (authentication.getPrincipal() instanceof DefaultOidcUser) {
+      Map<String, Object> attributes = ((DefaultOidcUser) authentication.getPrincipal()).getAttributes();
+      if (attributes.containsKey("preferred_username")) {
+        return (String) attributes.get("preferred_username");
+      }
+    } else if (authentication.getPrincipal() instanceof String) {
+      return (String) authentication.getPrincipal();
+    }
+    return null;
+  }
+
+  /**
+   * Check if a user is authenticated.
+   *
+   * @return true if the user is authenticated, false otherwise.
+   */
+  public static boolean isAuthenticated() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    return authentication != null;
+  }
+
+  /**
+   * If the current user has a specific authority (security role).
+   * <p>
+   * The name of this method comes from the {@code isUserInRole()} method in the Servlet API.
+   *
+   * @param authority the authority to check.
+   * @return true if the current user has the authority, false otherwise.
+   */
+  public static boolean isCurrentUserInRole(String authority) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    return authentication != null &&
+      getAuthorities(authentication).anyMatch(authority::equals);
+  }
+
+  private static Stream<String> getAuthorities(Authentication authentication) {
+    Collection<? extends GrantedAuthority> authorities = authentication instanceof JwtAuthenticationToken ?
+      extractAuthorityFromClaims(((JwtAuthenticationToken) authentication).getToken().getClaims())
+      : authentication.getAuthorities();
+    return authorities.stream()
+      .map(GrantedAuthority::getAuthority);
+  }
+
+  public static List<GrantedAuthority> extractAuthorityFromClaims(Map<String, Object> claims) {
+    return mapRolesToGrantedAuthorities(getRolesFromClaims(claims));
+  }
+
+  @SuppressWarnings("unchecked")
+  private static Collection<String> getRolesFromClaims(Map<String, Object> claims) {
+    return (Collection<String>) claims.getOrDefault("groups",
+      claims.getOrDefault("roles", new ArrayList<>()));
+  }
+
+  private static List<GrantedAuthority> mapRolesToGrantedAuthorities(Collection<String> roles) {
+    return roles.stream()
+      .filter(role -> role.startsWith("ROLE_"))
+      .map(SimpleGrantedAuthority::new)
+      .collect(Collectors.toList());
+  }
+
+  public Jwt currentAccessToken() {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    return (Jwt) auth.getCredentials();
+  }
+}
