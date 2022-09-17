f3c32a0fcd244c26e123097a6c4866399e3ec880
Abdelouahedd <ennouriabdelouahed@gmail.com>
2022-09-17 15:23:22 +0100

    feat(docker): complete configuration of dockerized all services

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-mvn clean compile
-mvn package
-mvn install
+#mvn clean compile
+mvn clean -DskipTests package
+
 
 cd docker || exit
 docker-compose -f app.yml up --build --remove-orphans
